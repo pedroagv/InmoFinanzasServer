0694bd611f6fb5d231f13b79f92a0f15b8b443fc
pedroagv <escomoloquiera@gmail.com>
2024-06-09 21:40:27 -0500

    firebase upload  env

--- firebase.py
+++ firebase.py
@@ -1,9 +1,28 @@
+import os
+import json
+from dotenv import load_dotenv
 import firebase_admin
 from firebase_admin import credentials, firestore
 
+# Cargar las variables de entorno desde el archivo .env
+load_dotenv()
+
 def get_firestore_client():
     if not firebase_admin._apps:
-        cred = credentials.Certificate('inmofinanzafb-firebase.json')
-        firebase_admin.initialize_app(cred)
-    return firestore.client()
+        # Obtener el contenido del archivo JSON desde la variable de entorno
+        firebase_credentials = os.getenv('FIREBASE_CREDENTIALS')
         
+        if firebase_credentials:
+            # Convertir la cadena JSON a un diccionario
+            firebase_credentials_dict = json.loads(firebase_credentials)
+
+            # Crear una credencial de Firebase a partir del diccionario
+            cred = credentials.Certificate(firebase_credentials_dict)
+            
+            # Inicializar la aplicación de Firebase
+            firebase_admin.initialize_app(cred)
+        else:
+            print("Las credenciales de Firebase no se encontraron en las variables de entorno.")
+            return None
+    
+    return firestore.client()
