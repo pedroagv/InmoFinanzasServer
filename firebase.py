cc4c7d342d012e102b4428b6ebdc5ed9443685ba
pedroagv <escomoloquiera@gmail.com>
2024-06-09 21:08:08 -0500

    probando desde aca

---
+++ firebase.py
@@ -0,0 +1,9 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+def get_firestore_client():
+    if not firebase_admin._apps:
+        cred = credentials.Certificate('inmofinanzafb-firebase.json')
+        firebase_admin.initialize_app(cred)
+    return firestore.client()
+    
