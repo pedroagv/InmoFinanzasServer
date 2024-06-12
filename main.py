0815ffa7ef1b1654923f1633ea2985f609acc7c7
pedroagv <escomoloquiera@gmail.com>
2024-06-11 20:48:38 -0500

    ajuste descarga de iamgenes

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from app.api.endpoints import productos, categorias
+from fastapi.responses import FileResponse
 from typing import List
 import os
 
@@ -27,6 +28,16 @@ app.include_router(categorias.router)
 def hello_world():
     return '¡Primera deploy en Python'
 
+@app.get("/uploads/{filename}")
+async def get_image(filename: str):
+    try:
+        # La ruta completa de la imagen
+        file_path = f"uploads/{filename}"
+        # Devolver la imagen
+        return FileResponse(file_path)
+    except Exception as e:
+        return str(e), 404
+
 
 @app.post("/upload")
 async def upload_files(files: List[UploadFile] = File(...)):
@@ -38,11 +49,7 @@ async def upload_files(files: List[UploadFile] = File(...)):
         with open(file_path, "wb") as f:
             f.write(file.file.read())
         file_paths.append(file_path)
-    # Construir las URL de las imágenes subidas
-    base_url = "https://inmofinanzasserver.onrender.com/upload/" 
-    image_urls = [base_url + os.path.basename(file_path) for file_path in file_paths]
-    return JSONResponse(content={"message": "Files uploaded successfully", "file_paths": file_paths, "image_urls": image_urls})
-
+    return JSONResponse(content={"message": "Files uploaded successfully", "file_paths": file_paths})
 
 if __name__ == '__main__': 
     import uvicorn
