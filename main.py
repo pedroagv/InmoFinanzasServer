2129c56f6e95e3123009a694d6c02a2caf02f2f7
pedroagv <escomoloquiera@gmail.com>
2024-06-20 21:36:29 -0500

    mejoras supersonicas al sitio

--- main.py
+++ main.py
@@ -27,48 +27,16 @@ app.include_router(archivos.router)
 
 # app = Flask(__name__)
 
-# @app.get('/')
-# def hello_world():
-#     # Obtener la hora actual
-#     now = datetime.now()
-#     current_time = now.strftime("%H:%M:%S")  # Formato HH:MM:SS
-    
-#     # Saludo que quieres retornar
-#     greeting = '¡Api InmofinanzasAGV en Python! '
-    
-#     # Concatenar el saludo con la hora actual
-#     message = greeting + 'La hora actual es ' + current_time
-    
-#     return message
-
-# @app.get("/uploads/{folder}/{filename}")
-# async def get_image(folder: str, filename: str):
-#     try:
-#         UPLOADS_DIR = Path("uploads")
-#         # Construir la ruta completa de la imagen
-#         file_path = UPLOADS_DIR / folder / filename
-#         # Verificar si el archivo existe
-#         if not file_path.is_file():
-#             return "Imagen no encontrada", 404
-#         # Devolver la imagen
-#         return FileResponse(file_path)
-#     except Exception as e:
-#         return str(e), 500
-
-# @app.post("/upload")
-# async def upload_files(folder: str, files: List[UploadFile] = File(...)):
-#     folder_path = os.path.join(UPLOAD_FOLDER, folder)
-#     if not os.path.exists(folder_path):
-#         os.makedirs(folder_path)
-    
-#     file_paths = []
-#     for file in files:
-#         file_path = os.path.join(folder_path, file.filename)
-#         with open(file_path, "wb") as f:
-#             f.write(file.file.read())
-#         file_paths.append(file_path)
-    
-#     return JSONResponse(content={"message": "Files uploaded successfully", "file_paths": file_paths})
+@app.get('/')
+def hello_world():
+    # Obtener la hora actual
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")  # Formato HH:MM:SS    
+    # Saludo que quieres retornar
+    greeting = '¡Api InmofinanzasAGV en Python! '    
+    # Concatenar el saludo con la hora actual
+    message = greeting + 'La hora actual es ' + current_time    
+    return message
 
 if __name__ == '__main__': 
     import uvicorn
